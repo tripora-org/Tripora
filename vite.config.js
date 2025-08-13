627b0b58f3c56d20e28e4567ba0be1df734e9d7d
lian-004 <108578182+lian-004@users.noreply.github.com>
2025-08-13 17:40:57 +0200

    Troubleshoot

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/', // genau wie Ihr Repo heißt
+  build: {
+    outDir: 'docs' // Äquivalent zu --output-path docs
+  },
+  base: '/', // für Custom Domain
 })
